3e916fc1667f228b71bfee9e51bd637698b47a79
JaretWright <jaret.wright@georgiancollege.ca>
2021-01-26 14:57:11 -0500

    Updated the Kotlin main file to handle the calculations

--- app/src/main/java/com/example/w21comp3025w3/MainActivity.kt
+++ app/src/main/java/com/example/w21comp3025w3/MainActivity.kt
@@ -4,11 +4,17 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.SeekBar
+import androidx.core.widget.addTextChangedListener
 import com.example.w21comp3025w3.databinding.ActivityMainBinding
+import java.nio.channels.SeekableByteChannel
+import java.text.NumberFormat
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+    private var tipPercent = 15
+    private var initialMealCost = 0.0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -22,6 +28,46 @@ class MainActivity : AppCompatActivity() {
             var intent = Intent(this, SecondActivity::class.java)
             startActivity(intent)
         }
+
+        calculateTip()
+
+        //setup a click listener for the meal total field
+        binding.preTipMealTotalEditText.addTextChangedListener{
+            if (!it.isNullOrBlank())
+                initialMealCost = it.toString().toDouble()
+            else
+                initialMealCost = 0.0
+
+            calculateTip()
+        }
+
+        //configure a listener for the seekbar
+        binding.tipPercentSeekBar.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener{
+            override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
+                tipPercent = progress
+
+                calculateTip()
+            }
+
+            override fun onStartTrackingTouch(seekBar: SeekBar?) {
+            }
+
+            override fun onStopTrackingTouch(seekBar: SeekBar?) {
+            }
+
+        })
+    }
+
+    private fun calculateTip()
+    {
+        //calculate the tip
+        var tip = initialMealCost*tipPercent/100
+
+        //handle the presentation logic
+        val currencyFormat = NumberFormat.getCurrencyInstance()
+        binding.finalTipAmountTextView.text = currencyFormat.format(tip)
+        binding.totalMealCostTextView.text = currencyFormat.format(initialMealCost+tip)
+        binding.tipPrecentTextView.text = "${tipPercent}%"
     }
 
     override fun onStart(){
